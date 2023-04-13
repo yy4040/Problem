ebd7779fd8583524e7eeb10c9a98b4b3886550cc
Yong <yong@loadcomplete.com>
2023-04-13 20:52:00 +0900

    Template 코드 추가

---
+++ Template/Program.cs
@@ -0,0 +1,24 @@
+﻿var solver = new Solver(new ConsoleReader());
+var answer = solver.Solve();
+
+public interface IReader
+{
+    string ReadLine();
+}
+
+public class ConsoleReader : IReader
+{
+    public string ReadLine() => Console.ReadLine()!;
+}
+
+public class Solver
+{
+    public Solver(IReader reader)
+    {
+    }
+
+    public int Solve()
+    {
+        return 0;
+    }
+}
---
+++ Template/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿namespace Template;
+
+public class UnitTest1
+{
+    public void Test01()
+    {
+        var solver = new Solver(new TestReader01());
+        //Assert.That(solver.Solve(), Is.EqualTo(1));
+    }
+}
+
+public abstract class BaseTestReader : IReader
+{
+    private IEnumerator<string>? _enumerator;
+    
+    public string ReadLine()
+    {
+        _enumerator ??= GetEnumerator();
+        return _enumerator.MoveNext() ? _enumerator.Current : string.Empty;
+    }
+
+    protected abstract IEnumerator<string> GetEnumerator();
+}
+
+public class TestReader01 : BaseTestReader
+{
+    protected override IEnumerator<string> GetEnumerator()
+    {
+        yield return "Template";
+    }
+}
